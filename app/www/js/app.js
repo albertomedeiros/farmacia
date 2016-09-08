7346ae81d7d38dbc342a0b30637f9c677c11e5be
alberto <albertomedeiros214@gmail.cpm>
2016-09-07 23:34:18 -0300

    seguem atualizações do aplicativo

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -4,21 +4,12 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+var strUrlGoogle = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&latlng=";
+var objGetLocation = null;
 angular.module('starter', ['ionic', 'starter.controllers'])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+    
   });
 })
 
