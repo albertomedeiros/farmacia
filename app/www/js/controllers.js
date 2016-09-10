e73c77af63dc86ee6d8ffe5977cbc01cd0fc4896
alberto <albertomedeiros214@gmail.cpm>
2016-09-10 19:26:23 -0300

    segue atualizações

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -17,55 +17,90 @@ angular.module('starter.controllers', [])
     
     navigator.geolocation.getCurrentPosition(function(res) {
         objGetLocation = res.coords
-    }, function(err) {
-      console.log("deu erro...");
-      console.log(JSON.stringify(err));
-    })
+    }, function(err) {})
 
     if (window.StatusBar) {
       StatusBar.styleDefault();
     }
             
-  // Lista de Remédios
-  $scope.playlists = [
-    {valor: 15.60, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dipirona",      nome: 'Farmacia dos Pobres', id: 1 },
-    {valor: 6.35,  img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dipirona",      nome: 'Big Ben', id: 2 },
-    {valor: 7.4,   img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dorflex",       nome: 'Drogazil', id: 3 },
-    {valor: 6.38,  img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Paracetamol",   nome: 'Americanas', id: 4 },
-    {valor: 13.45, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Refesnoc",      nome: 'Carrefour', id: 5 },
-    {valor: 11.60, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Gripazil",      nome: 'Bom Preço', id: 6 }
-  ];
+    // Lista de Remédios
+    $scope.playlists = [
+      {valor: 15.60, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dipirona",      nome: 'Farmacia dos Pobres', id: 1 },
+      {valor: 6.35,  img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dipirona",      nome: 'Big Ben', id: 2 },
+      {valor: 7.4,   img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Dorflex",       nome: 'Drogazil', id: 3 },
+      {valor: 6.38,  img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Paracetamol",   nome: 'Americanas', id: 4 },
+      {valor: 13.45, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Refesnoc",      nome: 'Carrefour', id: 5 },
+      {valor: 11.60, img : "http://tecnomundoandroid.com.br/wp-content/uploads/2015/10/iFood-Delivery.png", remedio: "Gripazil",      nome: 'Bom Preço', id: 6 }
+    ];
   
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
+  
+    /***** MODAL DE INFORMAÇÕES DA FARMÁCIA ******/
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/informacoes.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modalFarmacia = modal;
+    });
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Triggered in the login modal to close it
+    $scope.closeModalFarmacia = function() {
+      $scope.modalFarmacia.hide();
+    };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    // Open the login modal
+    $scope.abrirFarmacia = function() {
+        $scope.modalFarmacia.show();
+    };
+    /***** MODAL DE INFORMAÇÕES DA FARMÁCIA ******/
+    /***** MODAL DE INFORMAÇÕES DO CARRINHO ******/
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/carrinhoFornecedor.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modalCarrinho = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeModalCarrinho = function() {
+      $scope.modalCarrinho.hide();
+    };
+
+    // Open the login modal
+    $scope.abrirCarrinho = function() {
+        console.log("caiu");
+        $scope.modalCarrinho.show();
+    };
+    /***** MODAL DE INFORMAÇÕES DA CARRINHO ******/
+    
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
 })
 .controller('PlaylistsCtrl', function($scope, $http) {
     var setIntervalGoogle = setInterval(function(){
@@ -92,9 +127,38 @@ angular.module('starter.controllers', [])
         objAtual = $scope.playlists[intI];
         if($stateParams.playlistId == objAtual.id){
             $scope.objAtual = objAtual;
-            console.log(objAtual);
         }        
     }
-    
-    $scope.finalizar = true;
+    // Conterá os remédios por fornecedor
+    $scope.arrCarrinho = [];
+    $scope.intTotalCarrinho = $scope.arrCarrinho.length;
+    // Função de adicionar remédio no carrinho
+    $scope.addCarrinho = function(parametro){
+        var intI = 0;
+        for(intI = 0 ; intI < $scope.playlists.length; intI++){
+            objAtual = $scope.playlists[intI];
+            if($stateParams.playlistId == objAtual.id){
+                $scope.arrCarrinho.push(objAtual);
+            }        
+        }
+        if($scope.arrCarrinho.length > 0){
+            $scope.finalizarCompra = true;
+        }
+        $scope.intTotalCarrinho = $scope.arrCarrinho.length;
+    };
+    // Função de remover do carrinho
+    $scope.removerCarrinho = function(){
+        var intI = 0;
+        for(intI = 0 ; intI < $scope.playlists.length; intI++){
+            objAtual = $scope.playlists[intI];
+            if($stateParams.playlistId == objAtual.id){
+                $scope.arrCarrinho.splice(intI);
+            }        
+        }
+        $scope.intTotalCarrinho = $scope.arrCarrinho.length;
+        console.log($scope.arrCarrinho.length);
+        if($scope.arrCarrinho.length == 0){
+            $scope.finalizarCompra = false;
+        }
+    };
 });
